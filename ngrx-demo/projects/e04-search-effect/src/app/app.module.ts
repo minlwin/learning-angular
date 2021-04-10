016d2e4031c4e7ae7f60ad63047aef6c57ce362a
minlwin <lwin.zawmin@gmail.com>
2021-04-10 22:32:43 +0630

    Update Location Effects Project

--- ngrx-demo/projects/e04-search-effect/src/app/app.module.ts
+++ ngrx-demo/projects/e04-search-effect/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { TownshipsEffects } from 'projects/e03-location-effect/src/app/states/location.effects';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DistrictComponent } from './district/district.component';
@@ -12,6 +11,7 @@ import { StateComponent } from './state/state.component';
 import { TownshipComponent } from './township/township.component';
 import { DistrictsEffects } from './utils/effects/districts.effects';
 import { StatesEffects } from './utils/effects/states.effects';
+import { TownshipsEffects } from './utils/effects/townships.effects';
 import { reduceAppState } from './utils/states/location.reducers';
 
 
--- ngrx-demo/projects/e04-search-effect/src/app/utils/effects/townships.effects.ts
+++ ngrx-demo/projects/e04-search-effect/src/app/utils/effects/townships.effects.ts
@@ -1,19 +1,18 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { map, mergeMap, tap } from "rxjs/operators";
+import { map, mergeMap } from "rxjs/operators";
 import { TownshipService } from "../services/township.service";
-import * as LocationActions from '../states/location.actions';
+import { TOWNSHIP_LOAD, TOWNSHIP_LOAD_SUCCESS } from "../states/location.actions";
 
 @Injectable()
 export class TownshipsEffects {
 
     constructor(private service: TownshipService, private actions$: Actions) { }
 
-    loadTownships$ = createEffect(() => this.actions$.pipe(
-        tap(data => console.log(data)),
-        ofType(LocationActions.TOWNSHIP_LOAD),
+    loadDistricts$ = createEffect(() => this.actions$.pipe(
+        ofType(TOWNSHIP_LOAD),
         mergeMap(params => this.service.search(params).pipe(
-            map(result => ({ type: LocationActions.TOWNSHIP_LOAD_SUCCESS.type, payload: result }))
+            map(result => ({ type: TOWNSHIP_LOAD_SUCCESS.type, payload: result }))
         ))
     ))
 }
